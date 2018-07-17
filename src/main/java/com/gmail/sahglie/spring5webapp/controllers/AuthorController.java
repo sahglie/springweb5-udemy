81acaf4d2dfce970047d01b2b1a31ed1e9464473
sahglie <sahglie@gmail.com>
2018-07-17 06:25:30 -0700

    wip

--- src/main/java/com/gmail/sahglie/spring5webapp/controllers/AuthorController.java
+++ src/main/java/com/gmail/sahglie/spring5webapp/controllers/AuthorController.java
@@ -4,7 +4,7 @@ import com.gmail.sahglie.spring5webapp.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthorController {
@@ -12,7 +12,7 @@ public class AuthorController {
     @Autowired
     private AuthorRepository authorRepository;
 
-    @RequestMapping(name = "/authors")
+    @GetMapping(path = "/authors")
     public String getAuthors(Model model) {
        model.addAttribute("authors", authorRepository.findAll());
        return "authors";
--- src/main/java/com/gmail/sahglie/spring5webapp/controllers/BookController.java
+++ src/main/java/com/gmail/sahglie/spring5webapp/controllers/BookController.java
@@ -4,14 +4,14 @@ import com.gmail.sahglie.spring5webapp.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-    @RequestMapping(name = "/books")
+    @GetMapping(path = "/books")
     public String getBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "books";
